fea4e75da84cf869c3edca7dbb8c98972603f07d
Jeong Jun Lee <gzookeeperlee1015@gmail.com>
2026-07-26 11:46:32 +0900

    [Passed] 배열 채우고 출력하기 (122ms, 10MB)

---
+++ trail0/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] a = new String[10];
+        for (int i = 0; i < a.length; i++) {
+    a[i] = sc.next();
+}
+    for (int i = a.length-1  ; i >= 0; i--) {
+        System.out.print(a[i]);
+    }
+}
+}

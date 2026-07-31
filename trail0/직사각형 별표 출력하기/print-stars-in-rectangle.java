375037ef5cfd3109c8b44897538c76ea3825665d
Jeong Jun Lee <gzookeeperlee1015@gmail.com>
2026-07-31 13:36:35 +0900

    [Passed] 직사각형 별표 출력하기 (132ms, 10MB)

---
+++ trail0/직사각형 별표 출력하기/print-stars-in-rectangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int m = sc.nextInt();
+       for ( int j = 0; j < n ; j++) {
+            for ( int i = 0; i < m ; i ++) {
+            System.out.print("* ");
+       }
+         System.out.println();
+       }
+    }
+}
